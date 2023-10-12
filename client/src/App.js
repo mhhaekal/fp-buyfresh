08bf9366c6b6f7a98647220f8b6b3198773d16fe
lensaBayu <bkprasetya@gmail.com>
2023-10-12 20:01:31 +0700

    Reusable_Component
    Navbar, Footer, Button and Product Card Done

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+//import component/pages
+import Navbar from "./components/navbarUser";
+import NavbarAdmin from "./components/navbarAdmin";
+import Footer from "./components/footer";
+import Button from "./components/button";
+import ProductCard from "./components/productCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div data-theme="light">
+
+      <Navbar />
+      {/* <NavbarAdmin /> */}
+      <div className="mt-[70px] flex flex-col justify-evenly p-5 lg:px-32 border">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-5 lg:gap-10 place-items-center">
+          <ProductCard name={"Buah Alpukat adasdadasd"} />
+          <ProductCard name={"Kiwi Hijau"} />
+          <ProductCard name={"Apel Fuji Premium"} />
+          <ProductCard name={"Blueberry Impor"} />
+          <ProductCard name={"Jeruk Mandarin Murcott"} />
+          <ProductCard name={"Kiwi Hijau"} />
+          <ProductCard name={"Kiwi Hijau"} />
+          <ProductCard name={"Kiwi Hijau"} />
+        </div>
+
+        <div className="mt-10"><Button text={"Confirm"} style={"w-[200px]"} /></div>
+
+
+      </div>
+      <Footer />
+
     </div>
   );
 }
